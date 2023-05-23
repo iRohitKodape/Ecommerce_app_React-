80a754d3778ef66469526c3e588f61bca4a545d4
iRohitKodape <rohitlkodape111@gmail.com>
2023-05-23 21:13:48 +0530

    routing setup completed

---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import Home from "./HomePage";
+import Products from "./ProductsPage";
+import Cart from "./CartPage";
+import About from "./AboutPage.js";
+import SingleProduct from "./SingleProductPage";
+import Error from "./ErrorPage";
+import Checkout from "./CheckoutPage";
+import Private from "./PrivateRoute";
+
+export { Home, Products, Cart, About, SingleProduct, Error, Checkout, Private };
